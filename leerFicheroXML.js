2eb3d93c190979b0864babf62d3070f2a90eb46e
Sergioguijarro87 <75782727+Sergioguijarro87@users.noreply.github.com>
2021-05-17 09:08:24 +0200

    Update leerFicheroXML.js

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -3,9 +3,9 @@ function gestionarFicheroXML(xmlDoc){
 	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
 		if (i == 2)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class="selecc">" + libros[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='selecc'>" + libros[i].textContent + "</p>"
 		else	
-			capaVacia.innerHTML = capaVacia.innerHTML + "<p class="bord">" + libros[i].textContent + "</p>"
+			capaVacia.innerHTML = capaVacia.innerHTML + "<p class='bord'>" + libros[i].textContent + "</p>"
 		
 	
 }
