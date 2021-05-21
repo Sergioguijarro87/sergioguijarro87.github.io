7534d6beda5fc24cc7b941eb915c5ce3e6465e82
Sergioguijarro87 <75782727+Sergioguijarro87@users.noreply.github.com>
2021-05-21 09:50:08 +0200

    Add files via upload

--- leerFicheroXML.js
+++ leerFicheroXML.js
@@ -1,12 +1,19 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
-	let libros = xmlDoc.querySelectorAll("libro")
-	for(let i=0; i<libros.length; i++)
+	let peliculas = xmlDoc.querySelectorAll("pelicula")
+	for(let i=0; i<peliculas.length; i++)
 		if (i == 2)
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='selecc'>" + libros[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='selecc'>" + peliculas[i].textContent + "</p>"
 		else	
-			capaVacia.innerHTML = capaVacia.innerHTML + "<p class='bord'>" + libros[i].textContent + "</p>"
+			capaVacia.innerHTML = capaVacia.innerHTML + "<p class='bord'>" + peliculas[i].textContent + "</p>"
 		
 	
 }
-	loadDocA("libros.xml","xml");
+
+let capa = document.querySelector("div:nth-child(1)") 
+capa.addEventListener("click",CargarFichero);
+function CargarFichero()
+{
+	loadDocA("peliculas.xml","xml");
+}
+//	loadDocA("peliculas.xml","xml");
