8a0754786b5c2660377374cab4f123d44d85767c
Sergioguijarro87 <75782727+Sergioguijarro87@users.noreply.github.com>
2021-05-21 09:39:23 +0200

    Add files via upload

--- examen/leerFicheroXML.js
+++ examen/leerFicheroXML.js
@@ -1,7 +1,7 @@
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
 	let peliculas = xmlDoc.querySelectorAll("pelicula")
-	for(let i=0; i<libros.length; i++)
+	for(let i=0; i<peliculas.length; i++)
 		if (i == 2)
 		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='selecc'>" + peliculas[i].textContent + "</p>"
 		else	
@@ -10,7 +10,7 @@ function gestionarFicheroXML(xmlDoc){
 	
 }
 
-let capa = document.querySelector("#ficheroXML") 
+let capa = document.querySelector("div:nth-child(1)") 
 capa.addEventListener("click",CargarFichero);
 function CargarFichero()
 {
